f4f372123a5303882183dbd07ff6650d2493ecb2
Rubyroy12 <ibrakipz7@gmail.com>
2021-04-23 02:36:51 +0300

    add javascript functions

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// business logic
+
+function Order(size, topping, crust) {
+    this.size = {};
+    this.topping = topping;
+    this.crust = crust;
+}
+
+function sizes(large, medium, small) {
+    this.large=large;
+    this.medium= medium;
+    this.small=small;
+}
+Order.prototype.sizes= function(){
+    return this.large + this.medium + this.small;
+}
+function  topping(type,price){
+    this.type=type;
+    this.price=price;
+}
+function crust(type,price){
+    this.type=type;
+    this.price=price;
+
+}
+$(document).ready(function(){
+    $("form#myform").submit(function(event){
+        event.preventDefault();
+        var inputtedsize= $("#size").val();
+        var inputtedtopping= $("#topping").val();
+        var inputtedcrust= $("#crust").val();
+
+        $("p#ordersize").append(inputtedsize);
+        $("p#ordertopping").append(inputtedtopping);
+        $("p#ordercrust").append(inputtedcrust);
+
+
+    })
+})
