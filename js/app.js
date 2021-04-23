35e07fc00639f8f932d30ec9a01125e53291d369
Rubyroy12 <ibrakipz7@gmail.com>
2021-04-23 18:05:30 +0300

    add jqeury event listeners

--- js/app.js
+++ js/app.js
@@ -9,15 +9,19 @@ function Order(size, topping, crust) {
 
 //  const size = new Order(large:2000, medium:1500, )
 Order.prototype.fullorder1 = function(){
-    return this.size + this.topping + this.crust;
+    return this.size + ", "+ this.topping +" ,"+ this.crust;
 }
-// var 
+ 
 // order.size= new size(large,medium,small);
+// var size = [large, medium, small];
+// var sprice = [2000,1500, 1000];
 
 // function sizes(large, medium, small) {
 //     this.large=large;
 //     this.medium= medium;
 //     this.small=small;
+// }
+// var l
 // }
 // Order.prototype.sizes= function(){
 //     return this.large + this.medium + this.small;
@@ -35,16 +39,25 @@ Order.prototype.fullorder1 = function(){
 
 // USER INTERFACE LOGIC
 $(document).ready(function(){
+    $("#makeorderbtn").on("click",function(){
+        $("form#myform").fadeIn();
+        $(this).text("Make another order");
+    })
+})
+$(document).ready(function(){
     $("form#myform").submit(function(event){
         event.preventDefault();
         var inputtedsize= $("#size").val();
         var inputtedtopping= $("#topping").val();
         var inputtedcrust= $("#crust").val();
-         var fullorder= new Order(inputtedsize, inputtedtopping, inputtedcrust)
-        // $("p#ordersize").append(inputtedsize);
+         var fullorder= new Order(inputtedsize , inputtedtopping, inputtedcrust)
+       
         // $("p#ordertopping").append(inputtedtopping);
         // $("p#ordercrust").append(inputtedcrust);
         $("p#fullorder").append(fullorder.fullorder1());
+        $("form#myform").slideUp();
+        
+
 
 
     })
