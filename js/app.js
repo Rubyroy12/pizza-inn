d89c2d0d47b07945d400fb8e0d6bd49ed1238c04
Rubyroy12 <ibrakipz7@gmail.com>
2021-04-23 17:00:13 +0300

    create a constructor instance

--- js/app.js
+++ js/app.js
@@ -2,27 +2,38 @@
 
 function Order(size, topping, crust) {
     this.size = {};
-    this.topping = topping;
-    this.crust = crust;
+    this.topping = {};
+    this.crust = {};
 }
+ 
 
-function sizes(large, medium, small) {
-    this.large=large;
-    this.medium= medium;
-    this.small=small;
+//  const size = new Order(large:2000, medium:1500, )
+Order.prototype.fullorder = function(){
+    return this.size + this.topping + this.crust;
 }
-Order.prototype.sizes= function(){
-    return this.large + this.medium + this.small;
-}
-function  topping(type,price){
-    this.type=type;
-    this.price=price;
-}
-function crust(type,price){
-    this.type=type;
-    this.price=price;
+// var 
+// order.size= new size(large,medium,small);
 
-}
+// function sizes(large, medium, small) {
+//     this.large=large;
+//     this.medium= medium;
+//     this.small=small;
+// }
+// Order.prototype.sizes= function(){
+//     return this.large + this.medium + this.small;
+// }
+// function  topping(type,price){
+//     this.type=type;
+//     this.price=price;
+// }
+// function crust(type,price){
+//     this.type=type;
+//     this.price=price;
+
+// }
+
+
+// USER INTERFACE LOGIC
 $(document).ready(function(){
     $("form#myform").submit(function(event){
         event.preventDefault();
@@ -37,3 +48,4 @@ $(document).ready(function(){
 
     })
 })
+
